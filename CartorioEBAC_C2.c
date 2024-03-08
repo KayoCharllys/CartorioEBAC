021af414a5a3c0a12d1ef4efa34828b8cf6a1d07
KayoCharllys <162705058+KayoCharllys@users.noreply.github.com>
2024-03-08 12:19:32 +0000

    Opção de saída
    Adição da opção para sair do sistema no menu inicial

--- CartorioEBAC_C2.c
+++ CartorioEBAC_C2.c
@@ -127,7 +127,8 @@ int main()
 	   printf("*Escolha a opção desejada do menu: \n\n");
 	   printf("\t1 - Registrar nomes \n");
 	   printf("\t2 - Consultar nomes \n");
-	   printf("\t3 - Deletar nomes \n\n"); 
+	   printf("\t3 - Deletar nomes \n");
+	   printf("\t4 - Sair do Sistema\n\n");
 	   printf("opção:");//fim do menu
 	
        scanf("%d", &opcao); //armazenando a escolha do usuário
@@ -146,6 +147,11 @@ int main()
 		   
 		   case 3:
 		   deletar();
+		   break;
+		   
+		   case 4:
+		   printf("Obrigado por usar o nosso sistema!\n");
+		   return 0;
 		   break;
 		   
 		   default:

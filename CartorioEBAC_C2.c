b338b1ac8a95c7ab7a4750ef11179ef49ddbb05a
KayoCharllys <162705058+KayoCharllys@users.noreply.github.com>
2024-03-08 20:26:56 +0000

    Opção voltar ao menu
    adicionamento da opção voltar ao menu em todas as funções(registrar, consultar, deletar) para facilitar para o usuário.

--- CartorioEBAC_C2.c
+++ CartorioEBAC_C2.c
@@ -5,7 +5,15 @@
 
 int registro()
 {
-    char arquivo[40];
+    int opcao=0;
+	int x=1;
+	
+	for(x=1;x=1;) //Operador de repetição
+	{
+	
+	setlocale(LC_ALL, "portuguese"); //Definindo a linguagem
+    
+	char arquivo[40];
 	char cpf[40];
     char nome[40];
     char sobrenome[40];
@@ -47,19 +55,45 @@ int registro()
     fprintf(file,", Cargo:");
     fclose(file);
     
-    printf("Digite o cargo a ser cadastrado: ");
+    printf("Digite o cargo a ser cadastrado:");
     scanf("%s", cargo);
     
     file = fopen(arquivo, "a");
     fprintf(file, cargo);
     fclose(file);
     
-    system("pause");
+    printf("\nDeseja adcionar mais usuários?\n\n"); //Opção ao usuario de voltar ao menu ou continuar adcionando
+    printf("\t1 - Sim\n");
+    printf("\t2 - Não\n");
+    printf("\n opção:");
+	
+       scanf("%d", &opcao); //armazenando a escolha do usuário
+    
+       system("cls");
+       
+        switch(opcao)
+        {
+		   case 1: //Inicio da seleção
+		   registro();
+    	   break;
+		   
+		   case 2:
+		   main();
+		   system("pause");
+		   break;
+	    }
+    }
 }
 
 int consulta()
 {
-    setlocale(LC_ALL, "portuguese"); //Definindo a linguagem
+    int opcao=0;
+	int x=1;
+	
+	for(x=1;x=1;) //Operador de repetição
+	{
+	
+	setlocale(LC_ALL, "portuguese"); //Definindo a linguagem
     
 	char cpf[40]; //Localizar o arquivo
     char conteudo[200];
@@ -72,21 +106,47 @@ int consulta()
     
     if(file == NULL) //Caso o Usuário digite um CPF não cadastrado
     {
-    	printf("Não foi possivel localizar o CPF consultado!.\n");
+    	printf("Não foi possivel localizar o CPF consultado!.\n\n");
 	}
 	while(fgets(conteudo, 200, file) != NULL)
 	{
 		printf("\nEssas são as informações do usuário: CPF:"); //Resposta para o Usuário
 		printf("%s", conteudo);
 		printf("\n\n");
+			
 	}
 	
-	system("pause");
-	fclose(file);
+	printf("Consultar outro CPF?\n\n"); //Opção ao usuario para voltar ao menu ou continuar consultando CPFs
+	printf("\t1 - Sim\n");
+	printf("\t2 - Não\n\n");
+	printf("Opção:");
+	
+	   scanf("%d", &opcao); //armazenando a escolha do usuário
+    
+       system("cls");
+       
+        switch(opcao)
+        {
+		   case 1: //Inicio da seleção
+		   consulta();
+    	   break;
+		   
+		   case 2:
+		   main();
+		   system("pause");
+		   break;
+	    }
+    	fclose(file);
+	}
 }
 
 int deletar()
 {
+    int opcao=0;
+	int x=1;
+	
+	for(x=1;x=1;) //Operador de repetição
+	{
 	
 	char cpf[40];
 	
@@ -101,13 +161,31 @@ int deletar()
 	
 	if(file == NULL); //Caso o CPF seja Nulo
 	{
-		printf("O CPF foi deletado com sucesso!.\n "); //Resposta ao usuário
-		system("pause");
+		printf("O CPF foi deletado com sucesso!.\n\n "); //Resposta ao usuário
 	}
 	
+	printf("Deletar outro CPF?\n\n"); //Opção para o usuario voltar ao menu ou continuar deletando CPFs
+	printf("\t1 - Sim\n");
+	printf("\t2 - Não\n\n");
+	printf("Opção:");
+	
+	   scanf("%d", &opcao); //armazenando a escolha do usuário
+    
+       system("cls");
+       
+        switch(opcao)
+        {
+		   case 1: //Inicio da seleção
+		   deletar();
+    	   break;
+		   
+		   case 2:
+		   main();
+		   system("pause");
+		   break;
+	    }
+    }
 }
-
-
 
 int main()
 {
@@ -117,10 +195,7 @@ int main()
 	for(x=1;x=1;) //Operador de repetição
 	{
 		system("cls");
-	
-	
-	
-	
+
        setlocale(LC_ALL, "portuguese"); //Definindo a linguagem
 	
 	   printf("\t---|Cartório da EBAC|--- \n\n"); //inicio do menu
@@ -151,7 +226,7 @@ int main()
 		   
 		   case 4:
 		   printf("Obrigado por usar o nosso sistema!\n");
-		   return 0;
+		   exit(0);
 		   break;
 		   
 		   default:
